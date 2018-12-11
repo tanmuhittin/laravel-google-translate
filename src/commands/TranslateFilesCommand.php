2616b345ef7391b56972f907bf91d1045dd3e3d0
Muhittin Tan <muhittin.tan@boun.edu.tr>
2018-12-11 11:17:58 +0300

    bug fix

--- src/commands/TranslateFilesCommand.php
+++ src/commands/TranslateFilesCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class TranslateFilesCommand extends Command
 {
-    public $locales,$base_locale,$excluded_files;
+    public $locales, $base_locale, $excluded_files;
     /**
      * The name and signature of the console command.
      *
@@ -40,26 +40,26 @@ class TranslateFilesCommand extends Command
     public function handle()
     {
         $this->base_locale = $this->option('baselocale');
-        $this->excluded_files = explode(",",$this->option('exclude'));
-        $bar = $this->output->createProgressBar((count($this->locales)-1));
+        $this->excluded_files = explode(",", $this->option('exclude'));
+        $bar = $this->output->createProgressBar((count($this->locales) - 1));
         $bar->start();
-        foreach ($this->locales as $locale){
-            if($locale == $this->base_locale)
+        foreach ($this->locales as $locale) {
+            if ($locale == $this->base_locale)
                 continue;
-            $this->line($this->base_locale." -> ".$locale." translating...");
-            $files = preg_grep('/^([^.])/', scandir(resource_path('lang/'.$this->base_locale)));
-            foreach ($files as $file){
-                $file = substr($file,0,-4);
-                if(in_array($file,$this->excluded_files))
+            $this->line($this->base_locale . " -> " . $locale . " translating...");
+            $files = preg_grep('/^([^.])/', scandir(resource_path('lang/' . $this->base_locale)));
+            foreach ($files as $file) {
+                $file = substr($file, 0, -4);
+                if (in_array($file, $this->excluded_files))
                     continue;
-                $to_be_translateds = trans($file,[],$this->base_locale);
+                $to_be_translateds = trans($file, [], $this->base_locale);
                 $new_lang = [];
-                foreach ($to_be_translateds as $key=>$to_be_translated){
-                    $new_lang[$key]=addslashes(self::translate($to_be_translated,$locale));
+                foreach ($to_be_translateds as $key => $to_be_translated) {
+                    $new_lang[$key] = addslashes(self::translate($to_be_translated, $locale));
                 }
                 //save new lang to new file
-                $file = fopen(resource_path('lang/'.$locale.'/'.$file.'.php'), "w+");
-                $write_text="<?php \nreturn " . var_export($new_lang, true) . ";";
+                $file = fopen(resource_path('lang/' . $locale . '/' . $file . '.php'), "w+");
+                $write_text = "<?php \nreturn " . var_export($new_lang, true) . ";";
                 fwrite($file, $write_text);
                 fclose($file);
             }
@@ -77,9 +77,10 @@ class TranslateFilesCommand extends Command
      * @return mixed
      * @throws \Exception
      */
-    private function translate($text,$locale){
+    private function translate($text, $locale)
+    {
         $apiKey = config('laravel_google_translate.google_translate_api_key');
-        $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source='.$this->base_locale.'&target='.$locale;
+        $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=' . $this->base_locale . '&target=' . $locale;
         $handle = curl_init();
         curl_setopt($handle, CURLOPT_URL, $url);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
@@ -90,8 +91,10 @@ class TranslateFilesCommand extends Command
         $responseDecoded = json_decode($response, true);
         curl_close($handle);
 
-        if(isset($responseDecoded['error'])){
-            $this->error( $responseDecoded['error'][0]["message"]."\n");
+        if (isset($responseDecoded['error'])) {
+            $this->error("Google Translate API returned error");
+            if(isset($responseDecoded["error"]["message"]))
+                $this->error($responseDecoded["error"]["message"]);
             exit;
         }
 
