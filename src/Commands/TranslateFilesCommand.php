89eccae1d44973820c24fe02cfca0cb3985b020c
buzzclue <43321373+buzzclue@users.noreply.github.com>
2019-11-28 00:36:40 +0500

    Add support for yandex translator.

--- src/Commands/TranslateFilesCommand.php
+++ src/Commands/TranslateFilesCommand.php
@@ -57,28 +57,28 @@ class TranslateFilesCommand extends Command
     public function handle()
     {
         //Collect input
-        $this->base_locale = $this->ask('What is base locale?',config('app.locale','en'));
-        $this->locales = array_filter(explode(",", $this->ask('What are the target locales? Comma seperate each lang key','tr,it')));
-        $should_force = $this->choice('Force overwrite existing translations?',['No','Yes'],'No');
+        $this->base_locale = $this->ask('What is base locale?', config('app.locale', 'en'));
+        $this->locales = array_filter(explode(",", $this->ask('What are the target locales? Comma seperate each lang key', 'tr,it')));
+        $should_force = $this->choice('Force overwrite existing translations?', ['No','Yes'], 'No');
         $this->force = false;
-        if($should_force === 'Yes'){
+        if ($should_force === 'Yes') {
             $this->force = true;
         }
-        $mode = $this->choice('Use text exploration and json translation or php files?',['json','php'],'php');
+        $mode = $this->choice('Use text exploration and json translation or php files?', ['json','php'], 'php');
         $this->json = false;
-        if($mode === 'json'){
+        if ($mode === 'json') {
             $this->json = true;
         }
-        if(!$this->json){
-            $this->target_files = array_filter(explode(",", $this->ask('Are there specific target files to translate only? ex: file1,file2','')));
-            foreach ($this->target_files as $key=>$target_file){
+        if (!$this->json) {
+            $this->target_files = array_filter(explode(",", $this->ask('Are there specific target files to translate only? ex: file1,file2', '')));
+            foreach ($this->target_files as $key=>$target_file) {
                 $this->target_files[$key] = $target_file.'.php';
             }
-            $this->excluded_files = array_filter(explode(",", $this->ask('Are there specific files to exclude?','auth,pagination,validation,passwords')));
+            $this->excluded_files = array_filter(explode(",", $this->ask('Are there specific files to exclude?', 'auth,pagination,validation,passwords')));
         }
-        $should_verbose = $this->choice('Verbose each translation?',['No','Yes'],'Yes');
+        $should_verbose = $this->choice('Verbose each translation?', ['No','Yes'], 'Yes');
         $this->verbose = false;
-        if($should_verbose === 'Yes'){
+        if ($should_verbose === 'Yes') {
             $this->verbose = true;
         }
         //Start Translating
@@ -86,7 +86,7 @@ class TranslateFilesCommand extends Command
         $bar->start();
         $this->line("");
         // loop target locales
-        if($this->json){
+        if ($this->json) {
             $this->line("Exploring strings...");
             $stringKeys = $this->explore_strings();
             $this->line('Exploration completed. Let\'s get started');
@@ -96,11 +96,10 @@ class TranslateFilesCommand extends Command
                 continue;
             }
             $this->line($this->base_locale . " -> " . $locale . " translating...");
-            if($this->json){
-                $this->translate_json_array_file($locale,$stringKeys);
-            }
-            else if ($locale !== 'vendor') {
-                if(!is_dir(resource_path('lang/' . $locale))){
+            if ($this->json) {
+                $this->translate_json_array_file($locale, $stringKeys);
+            } elseif ($locale !== 'vendor') {
+                if (!is_dir(resource_path('lang/' . $locale))) {
                     mkdir(resource_path('lang/' . $locale));
                 }
                 $this->translate_php_array_files($locale);
@@ -123,33 +122,34 @@ class TranslateFilesCommand extends Command
      */
     public static function translate($base_locale, $locale, $text)
     {
-        preg_match_all("/(^:|([\s|\:])\:)([a-zA-z])+/",$text,$matches);
+        preg_match_all("/(^:|([\s|\:])\:)([a-zA-z])+/", $text, $matches);
         $parameter_map = [];
         $i = 1;
-        foreach($matches[0] as $match){
+        foreach ($matches[0] as $match) {
             $parameter_map ["x".$i]= $match;
-            $text = str_replace($match," x".$i,$text);
+            $text = str_replace($match, " x".$i, $text);
             $i++;
         }
-        if(config('laravel_google_translate.google_translate_api_key', false)){
+        if (config('laravel_google_translate.google_translate_api_key', false) or config('laravel_google_translate.translator', false) == 'yandex') {
             $translated = self::translate_via_api_key($base_locale, $locale, $text);
-        }else{
+        } else {
             $translated = self::translate_via_stichoza($base_locale, $locale, $text);
         }
-        foreach ($parameter_map as $key=>$attribute){
+        foreach ($parameter_map as $key=>$attribute) {
             $combinations = [
                 $key,
-                substr($key,0,1)." ".substr($key,1),
-                strtoupper(substr($key,0,1))." ".substr($key,1),
-                strtoupper(substr($key,0,1)).substr($key,1)
+                substr($key, 0, 1)." ".substr($key, 1),
+                strtoupper(substr($key, 0, 1))." ".substr($key, 1),
+                strtoupper(substr($key, 0, 1)).substr($key, 1)
             ];
-            foreach ($combinations as $combination){
-                $translated = str_replace($combination,$attribute,$translated,$count);
-                if($count > 0)
+            foreach ($combinations as $combination) {
+                $translated = str_replace($combination, $attribute, $translated, $count);
+                if ($count > 0) {
                     break;
+                }
             }
         }
-        $translated = str_replace("  :"," :",$translated);
+        $translated = str_replace("  :", " :", $translated);
         return $translated;
     }
 
@@ -160,7 +160,8 @@ class TranslateFilesCommand extends Command
      * @return null|string
      * @throws \ErrorException
      */
-    private static function translate_via_stichoza($base_locale, $locale, $text){
+    private static function translate_via_stichoza($base_locale, $locale, $text)
+    {
         $tr = new GoogleTranslate();
         $tr->setSource($base_locale);
         $tr->setTarget($locale);
@@ -174,12 +175,22 @@ class TranslateFilesCommand extends Command
      * @return mixed
      * @throws \Exception
      */
-    private static function translate_via_api_key($base_locale, $locale, $text){
-        $apiKey = config('laravel_google_translate.google_translate_api_key', false);
-        $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=' . substr($base_locale, 0, strpos($base_locale."_", "_")) . '&target=' . substr($locale, 0, strpos($locale."_", "_"));
+    private static function translate_via_api_key($base_locale, $locale, $text)
+    {
+        $googleApiKey = config('laravel_google_translate.google_translate_api_key', false);
+        $yandexApiKey = config('laravel_google_translate.yandex_translate_api_key', false);
+        $translator = config('laravel_google_translate.translator', 'yandex');
+
+        if ($translator == 'google') {
+            $url = 'https://www.googleapis.com/language/translate/v2?key=' . $googleApiKey . '&q=' . rawurlencode($text) . '&source=' . substr($base_locale, 0, strpos($base_locale."_", "_")) . '&target=' . substr($locale, 0, strpos($locale."_", "_"));
+        } else {
+            $url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' . $yandexApiKey . '&text=' . rawurlencode($text) .'&lang=' . substr($base_locale, 0, strpos($base_locale."_", "_")) . '-' . substr($locale, 0, strpos($locale."_", "_"));
+        }
+
         $handle = curl_init();
         curl_setopt($handle, CURLOPT_URL, $url);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($handle);
         if ($response === false) {
             throw new \Exception(curl_error($handle), curl_errno($handle));
@@ -196,7 +207,7 @@ class TranslateFilesCommand extends Command
             exit;
         }
 
-        return $responseDecoded['data']['translations'][0]['translatedText'];
+        return $translator == 'google'? $responseDecoded['data']['translations'][0]['translatedText'] : $responseDecoded['text'][0];
     }
 
     /**
@@ -214,8 +225,9 @@ class TranslateFilesCommand extends Command
             $file = substr($file, 0, -4);
             $already_translateds = [];
             if (file_exists(resource_path('lang/' . $locale . '/' . $file . '.php'))) {
-                if($this->verbose)
+                if ($this->verbose) {
                     $this->line('File already exists: lang/' . $locale . '/' . $file . '.php. Checking missing translations');
+                }
                 $already_translateds = trans($file, [], $locale);
             }
             if (in_array($file, $this->excluded_files)) {
@@ -223,7 +235,7 @@ class TranslateFilesCommand extends Command
             }
             $to_be_translateds = trans($file, [], $this->base_locale);
             $new_lang = [];
-            if(is_array($to_be_translateds)){
+            if (is_array($to_be_translateds)) {
                 foreach ($to_be_translateds as $key => $to_be_translated) {
                     if (isset($already_translateds[$key]) && $already_translateds[$key] != '' && !$this->force) {
                         $new_lang[$key] = $already_translateds[$key];
@@ -232,12 +244,12 @@ class TranslateFilesCommand extends Command
                         }
                         continue;
                     }
-                    $new_lang[$key] = $this->translate_attribute($to_be_translated,$locale);
+                    $new_lang[$key] = $this->translate_attribute($to_be_translated, $locale);
                 }
             }
             //save new lang to new file
-            if(!file_exists(resource_path('lang/' . $locale ))){
-                mkdir(resource_path('lang/' . $locale ));
+            if (!file_exists(resource_path('lang/' . $locale))) {
+                mkdir(resource_path('lang/' . $locale));
             }
             $file = fopen(resource_path('lang/' . $locale . '/' . $file . '.php'), "w+");
             $write_text = "<?php \nreturn " . var_export($new_lang, true) . ";";
@@ -247,14 +259,15 @@ class TranslateFilesCommand extends Command
         return;
     }
 
-    private function translate_attribute($attribute,$locale){
-        if(is_array($attribute)){
+    private function translate_attribute($attribute, $locale)
+    {
+        if (is_array($attribute)) {
             $return = [];
-            foreach ($attribute as $k => $t){
-                $return[$k] = $this->translate_attribute($t,$locale);
+            foreach ($attribute as $k => $t) {
+                $return[$k] = $this->translate_attribute($t, $locale);
             }
             return $return;
-        }else{
+        } else {
             $translated = self::translate($this->base_locale, $locale, $attribute);
             if ($this->verbose) {
                 $this->line($attribute . ' : ' . $translated);
@@ -266,7 +279,8 @@ class TranslateFilesCommand extends Command
     /**
      * @return array
      */
-    public function explore_strings(){
+    public function explore_strings()
+    {
         $groupKeys  = [];
         $stringKeys = [];
         $functions  = config('laravel_google_translate.trans_functions', [
@@ -284,7 +298,7 @@ class TranslateFilesCommand extends Command
         ]);
         $groupPattern =                          // See https://regex101.com/r/WEJqdL/6
             "[^\w|>]" .                          // Must not have an alphanum or _ or > before real method
-            '(' . implode( '|', $functions ) . ')' .  // Must start with one of the functions
+            '(' . implode('|', $functions) . ')' .  // Must start with one of the functions
             "\(" .                               // Match opening parenthesis
             "[\'\"]" .                           // Match " or '
             '(' .                                // Start a new group to match:
@@ -295,26 +309,26 @@ class TranslateFilesCommand extends Command
             "[\),]";                            // Close parentheses or new parameter
         $stringPattern =
             "[^\w]" .                                     // Must not have an alphanum before real method
-            '(' . implode( '|', $functions ) . ')' .             // Must start with one of the functions
+            '(' . implode('|', $functions) . ')' .             // Must start with one of the functions
             "\(" .                                          // Match opening parenthesis
             "(?P<quote>['\"])" .                            // Match " or ' and store in {quote}
             "(?P<string>(?:\\\k{quote}|(?!\k{quote}).)*)" . // Match any string that can be {quote} escaped
             "\k{quote}" .                                   // Match " or ' previously matched
             "[\),]";                                       // Close parentheses or new parameter
         $finder = new Finder();
-        $finder->in( base_path() )->exclude( 'storage' )->exclude( 'vendor' )->name( '*.php' )->name( '*.twig' )->name( '*.vue' )->files();
+        $finder->in(base_path())->exclude('storage')->exclude('vendor')->name('*.php')->name('*.twig')->name('*.vue')->files();
         /** @var \Symfony\Component\Finder\SplFileInfo $file */
-        foreach ( $finder as $file ) {
+        foreach ($finder as $file) {
             // Search the current file for the pattern
-            if ( preg_match_all( "/$groupPattern/siU", $file->getContents(), $matches ) ) {
+            if (preg_match_all("/$groupPattern/siU", $file->getContents(), $matches)) {
                 // Get all matches
-                foreach ( $matches[ 2 ] as $key ) {
+                foreach ($matches[ 2 ] as $key) {
                     $groupKeys[] = $key;
                 }
             }
-            if ( preg_match_all( "/$stringPattern/siU", $file->getContents(), $matches ) ) {
-                foreach ( $matches[ 'string' ] as $key ) {
-                    if ( preg_match( "/(^[a-zA-Z0-9_-]+([.][^\1)\ ]+)+$)/siU", $key, $groupMatches ) ) {
+            if (preg_match_all("/$stringPattern/siU", $file->getContents(), $matches)) {
+                foreach ($matches[ 'string' ] as $key) {
+                    if (preg_match("/(^[a-zA-Z0-9_-]+([.][^\1)\ ]+)+$)/siU", $key, $groupMatches)) {
                         // group{.group}.key format, already in $groupKeys but also matched here
                         // do nothing, it has to be treated as a group
                         continue;
@@ -322,10 +336,10 @@ class TranslateFilesCommand extends Command
                     //TODO: This can probably be done in the regex, but I couldn't do it.
                     //skip keys which contain namespacing characters, unless they also contain a
                     //space, which makes it JSON.
-                    if ( !( mb_strpos( $key, '::' ) !== FALSE && mb_strpos( $key, '.' ) !==  FALSE )
-                        || mb_strpos( $key, ' ' ) !== FALSE ) {
+                    if (!(mb_strpos($key, '::') !== false && mb_strpos($key, '.') !==  false)
+                        || mb_strpos($key, ' ') !== false) {
                         $stringKeys[] = $key;
-                        if($this->verbose){
+                        if ($this->verbose) {
                             $this->line('Found : '.$key);
                         }
                     }
@@ -333,8 +347,8 @@ class TranslateFilesCommand extends Command
             }
         }
         // Remove duplicates
-        $groupKeys  = array_unique( $groupKeys ); // todo: not supporting group keys for now add this feature!
-        $stringKeys = array_unique( $stringKeys );
+        $groupKeys  = array_unique($groupKeys); // todo: not supporting group keys for now add this feature!
+        $stringKeys = array_unique($stringKeys);
         return $stringKeys;
     }
 
@@ -344,23 +358,23 @@ class TranslateFilesCommand extends Command
      * @throws \ErrorException
      * @throws \Exception
      */
-    public function translate_json_array_file($locale,$stringKeys)
+    public function translate_json_array_file($locale, $stringKeys)
     {
         $new_lang = [];
         $json_existing_translations = [];
-        if(file_exists(resource_path('lang/' . $locale . '.json'))){
+        if (file_exists(resource_path('lang/' . $locale . '.json'))) {
             $json_translations_string = file_get_contents(resource_path('lang/' . $locale . '.json'));
             $json_existing_translations = json_decode($json_translations_string, true);
         }
-        foreach ($stringKeys as $to_be_translated){
+        foreach ($stringKeys as $to_be_translated) {
             //check existing translations
-            if(isset($json_existing_translations[$to_be_translated]) &&
+            if (isset($json_existing_translations[$to_be_translated]) &&
                 $json_existing_translations[$to_be_translated]!='' &&
-                !$this->force)
-            {
+                !$this->force) {
                 $new_lang[$to_be_translated] = $json_existing_translations[$to_be_translated];
-                if($this->verbose)
+                if ($this->verbose) {
                     $this->line('Exists Skipping -> ' . $to_be_translated . ' : ' . $new_lang[$to_be_translated]);
+                }
                 continue;
             }
             $new_lang[$to_be_translated] = addslashes(self::translate($this->base_locale, $locale, $to_be_translated));
