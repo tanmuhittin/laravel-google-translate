34f60bd1486fd56181fd288999cd3b15be88da1b
neoteknic <neoteknic@gmail.com>
2021-09-08 01:56:39 +0200

    Update laravel_google_translate.php

--- src/config/laravel_google_translate.php
+++ src/config/laravel_google_translate.php
@@ -8,6 +8,7 @@ return [
         'no_requests_per_batch' => env('NO_REQUESTS_PER_BATCH', 5),
         'sleep_time_between_batches' => env('SLEEP_TIME_BETWEEN_BATCHES', 1)
     ],
+    'default_target_locales'=>'tr,it',
     'trans_functions' => [
         'trans',
         'trans_choice',
