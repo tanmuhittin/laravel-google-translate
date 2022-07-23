942494e11c1ad7b2536df4de6f285716f62a1618
Muhittin Tan <tanmuhittin@users.noreply.github.com>
2022-07-23 23:17:34 +0200

    Revert "Translate vendor language files (php) (#49)" (#57)
    This reverts commit 39c853e3b729bd02bee790e791ade4543fbccf96.

--- tests/Unit/TranslateFilesCommandTest.php
+++ tests/Unit/TranslateFilesCommandTest.php
@@ -22,10 +22,6 @@ class TranslateFilesCommandTest extends TestCase
         $this->assertFileExists(resource_path('lang/tr/tests.php'));
         unlink(resource_path('lang/tr/tests.php'));
         rmdir(resource_path('lang/tr'));
-
-        $this->assertFileExists(resource_path('lang/vendor/package/tr/vendor_tests.php'));
-        unlink(resource_path('lang/vendor/package/tr/vendor_tests.php'));
-        rmdir(resource_path('lang/vendor/package/tr'));
     }
 
     public function testTranslateJsonFilesCommand()
@@ -42,3 +38,4 @@ class TranslateFilesCommandTest extends TestCase
         unlink(resource_path('lang/tr.json'));
     }
 }
+
--- tests/test-resources/resources/lang/vendor/package/sv/vendor_tests.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    "text1" => "Tidpunkterna måste vara i formatet hh:mm! ",
-    "text2" => ":name har redan attesterat denna månad!",
-    "text3" => "Detta krockar med en registrering som :name har gjort mellan klockan :from och :to samma dag!",
-    "text4" => "Grattis, du hade rätt på :percent% av frågorna på första försöket!",
-    "text5" => "(Ange :alternatives alternativ)",
-];
