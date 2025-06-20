0ec707c3c27489224e9f4e0e12aed16c3baf3821
Vincent ⛄ Cardillo <vcardillo@gmail.com>
2025-06-20 01:33:48 -0700

    Clarifying the readme; Adding verbosity to doc block; adding output statements so we can be sure which translator is being used. (#75)
    Co-authored-by: Vincent Cardillo <vincent@givershealth.com>

--- src/Contracts/ApiTranslatorContract.php
+++ src/Contracts/ApiTranslatorContract.php
@@ -15,9 +15,9 @@ interface ApiTranslatorContract
 
 
     /**
-     * @param string $text
-     * @param string $locale
-     * @param string|null $base_locale
+     * @param string $text The text to be translated
+     * @param string $locale Language into which the text should be translated.
+     * @param string|null $base_locale Language of the source text to be translated. If omitted, some APIs will attempt to detect the source language automatically.
      * @return string
      */
     public function translate(string $text, string $locale, string $base_locale = null): string;
